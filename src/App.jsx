9748a1fd259704cb39f3410fa7a09e1500b05fe8
vaibhav <sanapvaibhav8767@gmail.com>
2025-07-31 20:50:27 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom"
+import Home from './Pages/Home'
 function App() {
   return (
     <>
-    <h1 className='text-bold mt-[45px]'>Vaibhav</h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+    </Router>
+
     </>
   )
 }
-
 export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='flex justify-around mt-[30px] '>
+      <div className='flex gap-[45px] list-none items-center'>
+        <h1 className='mr-[15px] text-2xl font-bold mt-[0px]'>Lando</h1>
+        <li className='font-semibold text-[19px]'>Home</li>
+        <li  className='font-semibold text-[19px]'>Pricing</li>
+        <li  className='font-semibold text-[19px]'>About us</li>
+        <li  className='font-semibold text-[19px]'>Contact Us</li>
+      </div>
+      <div className='flex list-none gap-[50px] items-center'>
+        <li  className='font-semibold text-[19px]' >Log in</li>
+        <li  className='font-semibold bg-blue-600 py-[8px] px-[17px] rounded-2xl text-white'>Sign Up</li>
+        </div>      
+    </div>
+  )
+}
+
+export default Navbar
