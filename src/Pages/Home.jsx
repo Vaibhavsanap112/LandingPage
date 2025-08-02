5e890687c69a4d87507d3fcddbb2c4997a4836cf
vaibhav <sanapvaibhav8767@gmail.com>
2025-08-02 18:30:26 +0530

    component3 done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
 import Component1 from '../Components/HomeComponents/Component1'
+import Component2 from '../Components/HomeComponents/Component2'
 
 function Home() {
   return (
     <div>
       <Component1></Component1>
+      <Component2></Component2>
     </div>
   )
 }
