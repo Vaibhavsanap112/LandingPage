f0399785547e68f6c64c8ed6a37af583c65fb6c6
vaibhav <sanapvaibhav8767@gmail.com>
2025-08-04 11:03:41 +0530

    added

---
+++ src/Components/HomeComponents/Component4.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Component4() {
+  return (
+   <div className="flex justify-evenly items-center">
+      <div>
+        <p className="font-bold text-2xl">
+          All your work is safe <br />
+          with us
+        </p>
+        <p className="mt-[19px]">We take your data security seriously, which is <br /> why we use advanced encryption protocols <br /> to protect your files in the cloud,<br /> Your data is safe and secure with us.  </p>
+        <p className="mt-[29px] border font-bold border-black w-fit p-[7px] px-[19px] rounded-[9px]">Try Now</p>
+      </div>
+      <div>
+        <img src="/Home/image4.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default Component4
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../Components/Navbar'
 import Component1 from '../Components/HomeComponents/Component1'
 import Component2 from '../Components/HomeComponents/Component2'
 import Component3 from '../Components/HomeComponents/Component3'
+import Component4 from '../Components/HomeComponents/Component4'
 
 function Home() {
   return (
@@ -10,6 +11,7 @@ function Home() {
       <Component1></Component1>
       <Component2></Component2>
       <Component3></Component3>
+      <Component4></Component4>
     </div>
   )
 }
